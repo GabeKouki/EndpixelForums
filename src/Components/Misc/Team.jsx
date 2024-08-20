b9fa72e62b0efff7cc6cd09ee54e2a2310a43741
Gabe Kouki <gabekouki02@gmail.com>
2024-08-20 16:06:19 -0400

    Fixed Errors from last commit

--- src/Components/Misc/Team.jsx
+++ src/Components/Misc/Team.jsx
@@ -1,43 +1,43 @@
 import './Team.css'
-import ReactSkinview3d from "react-skinview3d"
+// import ReactSkinview3d from "react-skinview3d"
 import React from "react";
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useState } from 'react';
+// import axios from 'axios';
+// import { useEffect } from 'react';
+// import { useState } from 'react';
 import LoginButton from '../Auth/LoginButton';
 import LogoutButton from '../Auth/LogoutButton';
 
 export default function Team() {
 
-    const [skinData, setSkinData] = useState([])
+    // const [skinData, setSkinData] = useState([])
 
-    const users = {
-        gabe: "BustyBannana"
-    }
+    // const users = {
+    //     gabe: "BustyBannana"
+    // }
 
-    useEffect(() => {
-        Object.values(users).map((username) => {
-            fetchUserData(username, Date.now())
-        })
+    // useEffect(() => {
+    //     Object.values(users).map((username) => {
+    //         fetchUserData(username, Date.now())
+    //     })
 
-    }, [])
+    // }, [])
 
-    const fetchUserData = async (username, timestamp) => {
-        const reqUrl = `https://api.mojang.com/users/profiles/minecraft/${username}?at=${timestamp}`;
+    // const fetchUserData = async (username, timestamp) => {
+    //     const reqUrl = `https://api.mojang.com/users/profiles/minecraft/${username}?at=${timestamp}`;
         
-        try {
-            const response = await axios.get(reqUrl);
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+    //     try {
+    //         const response = await axios.get(reqUrl);
+    //         const data = await response.json();
+    //         console.log(data);
+    //     } catch (error) {
+    //         console.error('Error fetching data:', error);
+    //     }
+    // };
     
 
-    const fetchUserSkin = async (username) => {
+    // const fetchUserSkin = async (username) => {
         
-    }
+    // }
 
     return(
 
